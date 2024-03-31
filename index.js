f20b9ddd1e9a18a40ad859706bbc95e44ece66b9
SyedAbdulSami1 <samiwpp@gmail.com>
2024-03-31 18:44:31 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+#! /usr/bin/env node;
+import inquirer from "inquirer";
+console.log(`Note: This is Just for testing Login Setup`);
+console.log(`Email: abc@gmail.com and Password: 1234`);
+console.log(`and also try with wrong Email or Password`);
+let email = "abc@gmail.com";
+let password = 1234;
+let clientInput = await inquirer.prompt([
+    {
+        name: "inputEmail",
+        message: "Enter Your Email: ",
+        type: "string",
+    },
+    {
+        name: "inputPassword",
+        message: "Enter Your Pasword: ",
+        type: "number",
+    },
+]);
+if (email === clientInput.inputEmail && password === clientInput.inputPassword) {
+    console.log("Welcome");
+    console.log("You are Successfully Login");
+}
+else if (email === clientInput.inputEmail && password != clientInput.inputPassword) {
+    console.log(`Your Password is incorrect. Please enter correct Password.`);
+}
+else {
+    console.log(`Invalid Eamil or Password`);
+}
+;
